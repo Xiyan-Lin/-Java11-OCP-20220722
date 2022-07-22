7fc1d9e375119a8f035ea8a9006e4bb00480ed3e
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-22 13:20:45 +0800

    java

--- src/main/java/com/day01/ArrayDemo4.java
+++ src/main/java/com/day01/ArrayDemo4.java
@@ -3,7 +3,7 @@ package com.day01;
 public class ArrayDemo4 {
     public static void main(String[] args) {
         int[] scores = {100, 83, 90, 70, 50};
-        // 分析 for-each(in)
+        // 分析 for-each(in) Java 1.4 開始支援
         // 計算總分與平均
         int sum = 0;  // 紀錄總分
         for(int score : scores) {
