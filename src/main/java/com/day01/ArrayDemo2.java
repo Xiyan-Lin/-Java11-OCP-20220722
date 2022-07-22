c25f1b4c793749329ec73c7090a0f0173ce4ade4
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-22 11:42:57 +0800

    java

--- src/main/java/com/day01/ArrayDemo2.java
+++ src/main/java/com/day01/ArrayDemo2.java
@@ -9,9 +9,19 @@ public class ArrayDemo2 {
         System.out.print(scores[2] + " ");
         System.out.print(scores[3] + " ");
         System.out.println(scores[4]);
-        // 使用 for-loop
+        // 使用 for-loop part I
         for(int i=0;i<scores.length;i++) {
             System.out.print(scores[i] + " ");
+        }
+        System.out.println();
+        // 使用 for-loop part II
+        for(int i=0, lens=scores.length ; i < lens ; i++) {
+            System.out.print(scores[i] + " ");
+        }
+        System.out.println();
+        // 使用 for-each(in) 不重視執行效率重視維護的狀況下使用
+        for(int score : scores) {          // for(int i=0;i<scores.length;i++)
+            System.out.print(score + " "); // System.out.print(scores[i] + " ");
         }
         System.out.println();
     }
