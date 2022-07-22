7101b07e52bb63ff13d83391ff6eaeffd0a325d4
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-22 13:29:55 +0800

    java

---
+++ src/main/java/com/day01/ArrayDemo5.java
@@ -0,0 +1,17 @@
+package com.day01;
+
+public class ArrayDemo5 {
+    public static void main(String[] args) {
+        int[] scores = {90, 83, 100, 50, 70};
+        // 求最高分(max) = ?
+        int max = 0;
+        for(int i=0 ; i < scores.length ; i++) {
+            if(scores[i] > max) {
+                max = scores[i];  // max 的內容換成 scores[i]
+            }
+        }
+        System.out.printf("最大值: %d\n", max);
+        // 求最低分(min) = ?
+        
+    }
+}
