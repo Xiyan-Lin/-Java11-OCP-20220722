9f5072f58001e9c6e800d366871439ca37011817
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-22 15:49:19 +0800

    java

---
+++ src/main/java/com/day01/ArrayDemo13.java
@@ -0,0 +1,13 @@
+package com.day01;
+
+import java.util.Arrays;
+
+public class ArrayDemo13 {
+    public static void main(String[] args) {
+        int[] scores = {90, 50, 100, 40}; // 4 筆
+        // 新加上 60 的分數, 再刪除第一筆資料
+        // [50, 100, 40, 60]
+        
+        
+    }
+}
