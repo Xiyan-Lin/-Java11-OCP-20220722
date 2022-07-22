98733b0ad4ddeef8ea5a4c317335dfb8e839e926
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-22 13:19:18 +0800

    java

---
+++ src/main/java/com/day01/ArrayDemo4.java
@@ -0,0 +1,16 @@
+package com.day01;
+
+public class ArrayDemo4 {
+    public static void main(String[] args) {
+        int[] scores = {100, 83, 90, 70, 50};
+        // 分析 for-each(in)
+        // 計算總分與平均
+        int sum = 0;  // 紀錄總分
+        for(int score : scores) {
+            sum += score; // sum = sum + score;
+        }
+        double avg = (double)sum/scores.length;
+        System.out.printf("總分: %d\n", sum);
+        System.out.printf("平均: %.1f\n", avg);
+    }
+}
