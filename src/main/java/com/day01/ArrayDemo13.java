160188d1c2eb8419cdac813fd297e3da682e1d71
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-22 16:00:15 +0800

    java

--- src/main/java/com/day01/ArrayDemo13.java
+++ src/main/java/com/day01/ArrayDemo13.java
@@ -7,7 +7,10 @@ public class ArrayDemo13 {
         int[] scores = {90, 50, 100, 40}; // 4 筆
         // 新加上 60 的分數, 再刪除第一筆資料
         // [50, 100, 40, 60]
-        
+        int[] scores2 = Arrays.copyOf(scores, scores.length + 1);
+        scores2[scores2.length - 1] = 60;
+        scores2 = Arrays.copyOfRange(scores2, 1, scores2.length);
+        System.out.println("scores2: " + Arrays.toString(scores2));
         
     }
 }
