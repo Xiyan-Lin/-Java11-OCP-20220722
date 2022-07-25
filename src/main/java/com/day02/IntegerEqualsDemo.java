8ad9947575a436bfb6aa31520e6839ddd1a90f3e
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 10:17:54 +0800

    java

--- src/main/java/com/day02/IntegerEqualsDemo.java
+++ src/main/java/com/day02/IntegerEqualsDemo.java
@@ -5,7 +5,8 @@ public class IntegerEqualsDemo {
         // Integer 與 int 的比較
         Integer x = 100;  // 因為 auto-boxing, 要看成 Integer.valueOf(100)
         int y = 100;
-        System.out.println("x == y: " + ( x == y ));
-        
+        System.out.println("x == y: " + ( x == y )); // javac 自動補全 x.intValue() 自動拆箱
+        System.out.println("x.intValue() == y: " + ( x.intValue() == y ));
+        System.out.println("x.equals(y): " + x.equals(y)); // javac 自動補全 Integer.valueOf(y) 自動裝箱
     }
 }
