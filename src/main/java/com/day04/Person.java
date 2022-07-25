15642ab222472545902bf680dc770b4c7a799385
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 15:54:27 +0800

    java

--- src/main/java/com/day04/Person.java
+++ src/main/java/com/day04/Person.java
@@ -6,6 +6,11 @@ public class Person {
     public Double weight;
     public Double bmi;
     
+    public void calcBmi() { // 計算 BMI 的方法
+        double bmiValue = weight / Math.pow(height/100, 2);
+        bmi = bmiValue;
+    }
+    
     public void print() {
         System.out.printf("Person height: %.1f, weight: %.1f, bmi: %.2f\n", height, weight, bmi);
     }
