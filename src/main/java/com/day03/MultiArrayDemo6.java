15d667b3a5ed147132e88ba83c646c0772ad7403
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 13:37:27 +0800

    java

--- src/main/java/com/day03/MultiArrayDemo6.java
+++ src/main/java/com/day03/MultiArrayDemo6.java
@@ -1,5 +1,7 @@
 package com.day03;
 
+import java.util.Arrays;
+
 public class MultiArrayDemo6 {
     public static void main(String[] args) {
         // 多維陣列分析
@@ -7,6 +9,18 @@ public class MultiArrayDemo6 {
         Integer[][] m = {{100, null, 80}, {Integer.parseInt("70"), 60, null}, {50, null, null}};
         // null 表示缺考
         // 印出所有考試分數(排除缺考)
-        
+        // 使用 for-each(in)
+        for(Integer[] array : m) {
+            for(Integer x : array) {
+                if(x != null) {
+                    System.out.println(x);
+                }
+            }
+        }
+        // 使用 Java 8 Stream
+        Arrays.stream(m)
+              .flatMap(array -> Arrays.stream(array))
+              .filter(x -> x != null)
+              .forEach(x -> System.out.println(x));
     }
 }
