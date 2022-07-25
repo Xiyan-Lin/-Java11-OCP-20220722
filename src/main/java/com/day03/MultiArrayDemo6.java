56013ae1eacfce9e1c133b3013953a2adfa99008
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 13:26:19 +0800

    java

---
+++ src/main/java/com/day03/MultiArrayDemo6.java
@@ -0,0 +1,12 @@
+package com.day03;
+
+public class MultiArrayDemo6 {
+    public static void main(String[] args) {
+        // 多維陣列分析
+        // Integer.parseInt("100") 會得到 100 (將字串轉 int)
+        Integer[][] m = {{100, null, 80}, {Integer.parseInt("70"), 60, null}, {50, null, null}};
+        // null 表示缺考
+        // 印出所有考試分數(排除缺考)
+        
+    }
+}
