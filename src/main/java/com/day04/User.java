9df7aaf3e83f4c2d46b32706349358507c9b4e7c
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 15:36:40 +0800

    java

--- src/main/java/com/day04/User.java
+++ src/main/java/com/day04/User.java
@@ -3,4 +3,8 @@ package com.day04;
 public class User {
     public String name; // 姓名(物件變數/欄位)
     public Integer age; // 年齡(物件變數/欄位)
+    
+    public void print() { // 提供一個列印資料的方法
+        System.out.printf("user name: %s, age: %d\n", name, age);
+    }
 }
