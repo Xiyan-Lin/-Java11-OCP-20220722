150226a586c5d94523aab29086094030a97c023c
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 15:56:16 +0800

    java

--- src/test/java/test/day04/PersonTest.java
+++ src/test/java/test/day04/PersonTest.java
@@ -7,13 +7,15 @@ public class PersonTest {
         Person person1 = new Person();
         person1.height = 170.0;
         person1.weight = 65.5;
-        person1.bmi = person1.weight / Math.pow(person1.height/100, 2);
+        //person1.bmi = person1.weight / Math.pow(person1.height/100, 2);
+        person1.calcBmi();
         person1.print();
         
         Person person2 = new Person();
         person2.height = 180.0;
         person2.weight = 95.5;
-        person2.bmi = person2.weight / Math.pow(person2.height/100, 2);
+        //person2.bmi = person2.weight / Math.pow(person2.height/100, 2);
+        person2.calcBmi();
         person2.print();
         
     }
