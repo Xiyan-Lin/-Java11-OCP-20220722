f5e32ffa920bb4bd1dd906a497c84de267ee8932
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 15:32:05 +0800

    java

--- src/test/java/test/day04/UserTest.java
+++ src/test/java/test/day04/UserTest.java
@@ -1,7 +1,17 @@
 package test.day04;
 
-
+import com.day04.User;
 
 public class UserTest {
-    
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.name = "John";
+        user1.age = 18;
+        System.out.printf("user1 name: %s, age: %d\n", user1.name, user1.age);
+        
+        User user2 = new User();
+        user2.name = "Mary";
+        user2.age = 19;
+        System.out.printf("user2 name: %s, age: %d\n", user2.name, user2.age);
+    }
 }
