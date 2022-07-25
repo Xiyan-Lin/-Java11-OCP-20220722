be463e18d76833697faf69b590300af1cd92c6ee
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-25 14:32:17 +0800

    java

--- src/main/java/com/day03/MultiArrayDemo8.java
+++ src/main/java/com/day03/MultiArrayDemo8.java
@@ -13,6 +13,11 @@ public class MultiArrayDemo8 {
         // 有名字三組請計算出平均名字的字數
         String[] names = {"Vincent", "Anita", "Bob"};
         //                   7個字     5個字   3個字
-        
+        double avg = Arrays.stream(names)  // {"Vincent", "Anita", "Bob"}
+                           //.mapToInt(name -> name.length()) // {7, 5, 3}
+                           .mapToInt(String::length) // {7, 5, 3}
+                           .average()
+                           .getAsDouble();
+        System.out.println(avg);        
     }
 }
