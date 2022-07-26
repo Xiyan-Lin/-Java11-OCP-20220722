96760a577d64e6872e7ec45c70dee2122d6848d4
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 15:19:13 +0800

    java

---
+++ src/main/java/com/day05/Calc.java
@@ -0,0 +1,16 @@
+package com.day05;
+// 四則運算的數學工具
+public class Calc {
+    public int add(int x, int y) {
+        return x + y;
+    }
+    public int sub(int x, int y) {
+        return x - y;
+    }
+    public int mul(int x, int y) {
+        return x * y;
+    }
+    public double div(int x, int y) {
+        return (double)x / y;
+    }
+}
