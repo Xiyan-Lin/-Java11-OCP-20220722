87800fc90efeb8d7f1461035d2f3d819a7a1ed98
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 15:27:10 +0800

    java

---
+++ src/test/java/test/day05/CalcTest.java
@@ -0,0 +1,15 @@
+package test.day05;
+
+import com.day05.Calc;
+
+public class CalcTest { 
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        System.out.println(Calc.add(x, y));
+        System.out.println(Calc.mul(x, y));
+        
+        
+        
+    }
+}
