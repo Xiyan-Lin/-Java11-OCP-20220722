2252368ddd7cfab3aa0b3ee5388efa8707c4e4f5
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 14:29:18 +0800

    java

--- src/main/java/com/day05/AccountSystem.java
+++ src/main/java/com/day05/AccountSystem.java
@@ -15,16 +15,25 @@ public class AccountSystem {
     
     // 2. 指定帳戶存款
     public void deposit(int index, Integer amount) {
+        if(!exists(index)) { // 若指定帳戶不存在
+            return; // 離開此方法
+        }
         accounts.get(index).deposit(amount);
     }
     
     // 3. 指定帳戶提款
     public void withdraw(int index, Integer amount) {
+        if(!exists(index)) { // 若指定帳戶不存在
+            return; // 離開此方法
+        }
         accounts.get(index).withdraw(amount);
     }
     
     // 4. 指定轉帳
     public void transfer(Integer amount, int fromIndex, int toIndex) {
+        if(!exists(fromIndex) || !exists(toIndex)) { // 若任一帳戶不存在
+            return; // 離開此方法
+        }
         Account from = accounts.get(fromIndex);
         Account to = accounts.get(toIndex);
         from.transfer(amount, to);
@@ -32,6 +41,9 @@ public class AccountSystem {
     
     // 5. 指定帳戶看餘額
     public void printAccount(int index) {
+        if(!exists(index)) { // 若指定帳戶不存在
+            return; // 離開此方法
+        }
         accounts.get(index).print();
     }
     
