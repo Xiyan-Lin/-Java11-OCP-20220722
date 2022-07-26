2027640d532b1e43e3b53358a15ab259e746dfca
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 13:51:54 +0800

    java

---
+++ src/test/java/test/day05/AccountSystemTest.java
@@ -0,0 +1,14 @@
+package test.day05;
+
+import com.day05.AccountSystem;
+
+public class AccountSystemTest {
+    public static void main(String[] args) {
+        AccountSystem system = new AccountSystem();
+        system.addAccount(10000);
+        system.addAccount(5000);
+        system.addAccount(7000);
+        
+        system.printAllAccounts();
+    }
+}
