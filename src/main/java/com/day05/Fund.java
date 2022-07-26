45f7edbb72ec23ed97a31eab794c58317611451d
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 15:49:21 +0800

    java

---
+++ src/main/java/com/day05/Fund.java
@@ -0,0 +1,28 @@
+package com.day05;
+
+// 募款基金
+public class Fund {
+    // 帳戶餘額
+    private Integer balance;
+    
+    // 自行先建立好捐款帳戶物件
+    private static Fund fund = new Fund();
+    
+    private Fund() {
+    }
+    
+    public static Fund getFund() {
+        return fund;
+    }
+    
+    // 捐款
+    public void donate(Integer amount) {
+        balance += amount;
+    }
+    
+    // 列印
+    public void print() {
+        System.out.printf("Fund balance: %d\n", balance);
+    }
+    
+}
