9167d7b22d5a4d05823a63417f34059f63897d3f
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 09:57:20 +0800

    java

---
+++ src/test/java/test/day05/BMITest.java
@@ -0,0 +1,10 @@
+package test.day05;
+
+import com.day05.BMI;
+
+public class BMITest {
+    public static void main(String[] args) {
+        BMI bmi1 = new BMI("John", 170.0, 60.0);
+        bmi1.print();
+    }
+}
