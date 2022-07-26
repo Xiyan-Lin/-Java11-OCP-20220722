1f76d1df25c8a2bba0525feca9b3f9e88689a874
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-26 15:52:58 +0800

    java

--- src/main/java/com/day05/Fund.java
+++ src/main/java/com/day05/Fund.java
@@ -3,7 +3,7 @@ package com.day05;
 // 募款基金
 public class Fund {
     // 帳戶餘額
-    private Integer balance;
+    private Integer balance = 0;
     
     // 自行先建立好捐款帳戶物件
     private static Fund fund = new Fund();
@@ -22,7 +22,7 @@ public class Fund {
     
     // 列印
     public void print() {
-        System.out.printf("Fund balance: %d\n", balance);
+        System.out.printf("Fund balance: %,d\n", balance);
     }
     
 }
---
+++ src/test/java/test/day05/FundTest.java
@@ -0,0 +1,20 @@
+package test.day05;
+
+import com.day05.Fund;
+
+public class FundTest {
+    public static void main(String[] args) {
+        Fund fund = Fund.getFund();
+        fund.donate(1000);
+        
+        Fund fund2 = Fund.getFund();
+        fund2.donate(5000);
+        
+        Fund fund3 = Fund.getFund();
+        fund3.donate(7000);
+        
+        fund.print();
+        fund2.print();
+        fund3.print();
+    }
+}
