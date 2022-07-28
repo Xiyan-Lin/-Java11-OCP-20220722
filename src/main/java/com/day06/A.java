7715fa010cb8e9f9e798e5f649ffe598e49a6dad
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-28 11:11:58 +0800

    java

--- src/main/java/com/day06/A.java
+++ src/main/java/com/day06/A.java
@@ -1,7 +1,12 @@
 package com.day06;
 
 public class A { // 父類
-    
+    public A() {
+        System.out.println("A1");
+    }
+    public A(int x) {
+        System.out.println("A2");
+    }
 }
 
 
--- src/main/java/com/day06/B.java
+++ src/main/java/com/day06/B.java
@@ -1,5 +1,10 @@
 package com.day06;
 
-public class B { // 子類
-    
+public class B extends A { // 子類
+    public B() {
+        System.out.println("B1");
+    }
+    public B(int x) {
+        System.out.println("B2");
+    }
 }
