3712dae54eba529d89efb125e0127b6f917c4144
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-28 10:13:32 +0800

    java

--- src/main/java/com/day06/Son.java
+++ src/main/java/com/day06/Son.java
@@ -1,5 +1,8 @@
 package com.day06;
 
 public class Son extends Father {
-    
+    @Override // 覆寫
+    public void printTitle() { // 取得職銜
+        System.out.println("ABC 股份有限公司 Java 工程師");
+    }
 }
