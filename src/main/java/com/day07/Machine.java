009d6a57f2a40975aca48684bf2ea683343d8f25
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-28 13:51:01 +0800

    java

---
+++ src/main/java/com/day07/Machine.java
@@ -0,0 +1,14 @@
+package com.day07;
+
+public class Machine {
+    private int power; // 瓦數
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+    
+}
