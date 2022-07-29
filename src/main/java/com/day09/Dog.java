d0c65cb1d74bba3ff1fbae79aea86a2d71694373
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-29 13:59:00 +0800

    java

---
+++ src/main/java/com/day09/Dog.java
@@ -0,0 +1,8 @@
+package com.day09;
+
+// Dog 介面(接口/規格)
+public interface Dog {
+    int feet = 4; // public static final int feet = 4;
+    void play();  // public abstract void play();
+    void eat();   // public abstract void eat();
+}
