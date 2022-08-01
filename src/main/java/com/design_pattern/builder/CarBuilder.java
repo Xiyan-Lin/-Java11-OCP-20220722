f6ea4d00225cd27c996e71f8798961a06ae4e3c7
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-01 09:56:31 +0800

    java

--- src/main/java/com/design_pattern/builder/CarBuilder.java
+++ src/main/java/com/design_pattern/builder/CarBuilder.java
@@ -1,5 +1,9 @@
 package com.design_pattern.builder;
 
 public interface CarBuilder {
-    
+    CarBuilder setWheel(Integer wheel);
+    CarBuilder setDoor(Integer door);
+    CarBuilder setCC(Integer cc);
+    CarBuilder setColor(String color);
+    Car create();
 }
--- src/main/java/com/design_pattern/builder/CarBuilderImpl.java
+++ src/main/java/com/design_pattern/builder/CarBuilderImpl.java
@@ -1,5 +1,40 @@
 package com.design_pattern.builder;
 
-public class CarBuilderImpl {
+public class CarBuilderImpl implements CarBuilder {
+    // 預設值
+    private Integer wheel = 4;
+    private Integer door = 2;
+    private Integer cc = 2000;
+    private String color = "Block";
+    
+    @Override
+    public CarBuilder setWheel(Integer wheel) {
+        this.wheel = wheel;
+        return this;
+    }
+
+    @Override
+    public CarBuilder setDoor(Integer door) {
+        this.door = door;
+        return this;
+    }
+
+    @Override
+    public CarBuilder setCC(Integer cc) {
+        this.cc = cc;
+        return this;
+    }
+
+    @Override
+    public CarBuilder setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    @Override
+    public Car create() {
+        Car car = new Car(wheel, door, cc, color);
+        return car;
+    }
     
 }
