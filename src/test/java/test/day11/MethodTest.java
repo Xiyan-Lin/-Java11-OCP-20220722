d6e6b08c76c8b9a5ec69ad7be617ae55263f1e90
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-01 15:56:58 +0800

    java

---
+++ src/test/java/test/day11/MethodTest.java
@@ -0,0 +1,10 @@
+package test.day11;
+
+import com.day11.Method;
+
+public class MethodTest {
+    public static void main(String[] args) {
+        Method method = new Method();
+        method.print();
+    }
+}
