507a48c46d1a3312560f6d64f7bd707b4e180970
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-01 13:56:30 +0800

    java

--- src/main/java/com/day11/Ball.java
+++ src/main/java/com/day11/Ball.java
@@ -1,5 +1,7 @@
 package com.day11;
 
+import java.util.Objects;
+
 public class Ball {
     private String color;
     private int price;
@@ -9,6 +11,51 @@ public class Ball {
         this.price = price;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.color);
+        hash = 89 * hash + this.price;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ball other = (Ball) obj;
+        if (this.price != other.price) {
+            return false;
+        }
+        return Objects.equals(this.color, other.color);
+    }
+    
+    
+    
+    /*
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Ball)) {
+            return false;
+        }
+        Ball ball = (Ball)obj;
+        return color.equals(ball.color) && price == ball.price;
+    }
+
+    @Override
+    public int hashCode() {
+        // 資料離散: 二個質數相乘再乘上或加上有比較的屬性資料
+        return 7 * 13 * price + color.hashCode();
+    }
+    */
+    
     public String getColor() {
         return color;
     }
--- src/test/java/test/day11/BallTest.java
+++ src/test/java/test/day11/BallTest.java
@@ -6,7 +6,11 @@ public class BallTest {
     public static void main(String[] args) {
         Ball b1 = new Ball("Red", 100);
         Ball b2 = new Ball("Red", 100);
+        System.out.println(b1);
+        System.out.println(b2);
         System.out.println(b1 == b2);
         System.out.println(b1.equals(b2));
+        System.out.println(b1.hashCode());
+        System.out.println(b2.hashCode());
     }
 }
