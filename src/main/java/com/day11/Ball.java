ce84817a16ef51cbdc20990e799f1b737654e5b6
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-01 13:09:13 +0800

    java

---
+++ src/main/java/com/day11/Ball.java
@@ -0,0 +1,34 @@
+package com.day11;
+
+public class Ball {
+    private String color;
+    private int price;
+
+    public Ball(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" + "color=" + color + ", price=" + price + '}';
+    }
+    
+    
+}
