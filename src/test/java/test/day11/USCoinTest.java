6f1fc8b7078194fd1d4d17d7f515727cb1e8e1df
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-01 14:26:04 +0800

    java

---
+++ src/test/java/test/day11/USCoinTest.java
@@ -0,0 +1,10 @@
+package test.day11;
+
+import com.day11.USCoin;
+
+public class USCoinTest {
+    public static void main(String[] args) {
+        USCoin usCoin = USCoin.NICKLE;
+        System.out.println(usCoin.getValue());
+    }
+}
