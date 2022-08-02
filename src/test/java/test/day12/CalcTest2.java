985714159fd7c5f19e4e7614e02b9a5aff977029
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-02 11:23:21 +0800

    java

--- src/test/java/test/day12/CalcTest2.java
+++ src/test/java/test/day12/CalcTest2.java
@@ -6,14 +6,17 @@ import java.util.Arrays;
 public class CalcTest2 {
     public static void main(String[] args) {
         // 陣列應用
-        Calc area = (r) -> Math.pow(r, 2) * Math.PI; // 計算圓面積公式
+        Calc area   = (r) -> Math.pow(r, 2) * Math.PI; // 計算圓面積公式
+        Calc volume = (r) -> 4/3.0 * Math.pow(r, 3) * Math.PI; // 計算球體積公式
+        Calc luckyNumber = (r) -> (int)(r * 1234.5678) % 10;
+        Calc calc = luckyNumber; // volume, area
         double[] array = {10, 15, 25.5, 30.5}; // 一堆半徑
         // case 1
         for(double r : array) {
-            System.out.println(area.get(r));
+            System.out.println(calc.get(r));
         }
         // case 2
-        Arrays.stream(array).forEach(r -> System.out.println(area.get(r)));
+        Arrays.stream(array).forEach(r -> System.out.println(calc.get(r)));
         
     }
 }
