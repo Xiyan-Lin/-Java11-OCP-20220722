8f04e2583a2bedf6db1038e2650872cb5acf1c51
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-02 09:39:44 +0800

    java

---
+++ src/test/java/test/day12/DogTest.java
@@ -0,0 +1,20 @@
+package test.day12;
+
+import com.day12.Dog;
+
+public class DogTest {
+    public static void main(String[] args) {
+        // 介面不可以直接 new
+        // Dog is abstract; cannot be instantiated
+        // Dog dog = new Dog();
+        
+        Dog dog = new Dog() {
+            @Override
+            public void skill(){
+                System.out.println("玩飛盤");
+            }
+        };
+        dog.skill();
+        
+    }
+}
