da2b70c1c0f59380320f0fbe0185af3183d1888e
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-02 11:05:39 +0800

    java

--- src/test/java/test/day12/CalcTest.java
+++ src/test/java/test/day12/CalcTest.java
@@ -22,5 +22,13 @@ public class CalcTest {
         Calc luckyNumber3 = (r) -> r * 1234 % 10;
         System.out.println(luckyNumber3.get(31.0));
         
+        // 計算圓面積
+        Calc area = (r) -> Math.pow(r, 2) * Math.PI;
+        System.out.println(area.get(10));
+        
+        // 計算球體積
+        Calc volume = (r) -> 4/3.0 * Math.pow(r, 3) * Math.PI;
+        System.out.println(volume.get(10));
+        
     }
 }
