bd67d988b5db4061ef3fbdf3b5f162561a04bbe7
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-02 11:33:30 +0800

    java

--- src/test/java/test/day12/Calc2Test.java
+++ src/test/java/test/day12/Calc2Test.java
@@ -10,5 +10,14 @@ public class Calc2Test {
         // 計算 bmi
         Calc2 bmi = (h, w) -> w / Math.pow(h/100, 2);
         System.out.println(bmi.get(170, 60));
+        
+        // 二維陣列
+        double[][] array = {
+            {170, 60},{180, 90},{160, 50}
+        };
+        // 1. 計算出每一個人的 bmi
+        
+        // 2. 求整體平均 bmi
+        
     }
 }
