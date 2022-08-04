b747123349e5cd3393281574a4d50e2b939c239e
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-04 15:14:42 +0800

    java

---
+++ src/main/java/com/day16/SetAndListDemo.java
@@ -0,0 +1,43 @@
+package com.day16;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Random;
+
+public class SetAndListDemo {
+    // 複合集合 List<Set<Integer>>
+    public static void main(String[] args) {
+        // 1. 建立 Set<Integer>
+        Set<Integer> lotto1 = getLottoSet();
+        Set<Integer> lotto2 = getLottoSet();
+        Set<Integer> lotto3 = getLottoSet();
+        // 2. 建立 List<Set<Integer>>
+        // 經過定義之後仍可自由變更數量
+        /*
+        List<Set<Integer>> lottos = new ArrayList<>();
+        lottos.add(lotto1);
+        lottos.add(lotto2);
+        lottos.add(lotto3);
+        */
+        // 一經過定義之後不可再變更數量
+        List<Set<Integer>> lottos = Arrays.asList(lotto1, lotto2, lotto3);
+        // 請問有幾注 ?
+        System.out.printf("注數: %d\n", lottos.size());
+        // 列出每一注資料
+        lottos.forEach(System.out::println);
+        // 列出每一注平均數值
+        
+    }
+    
+    private static Set getLottoSet() {
+        Random random = new Random();
+        Set<Integer> lotto = new LinkedHashSet<>();
+        while (lotto.size() < 5) {            
+            lotto.add(random.nextInt(39) + 1); // 1~39
+        }
+        return lotto;
+    }
+}
