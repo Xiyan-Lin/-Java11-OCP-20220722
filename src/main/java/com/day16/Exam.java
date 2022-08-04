6b4267a24d8d86f60c5e226295c6cbd629ffbcd2
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-04 15:48:32 +0800

    java

--- src/main/java/com/day16/Exam.java
+++ src/main/java/com/day16/Exam.java
@@ -1,5 +1,7 @@
 package com.day16;
 
+import java.util.Objects;
+
 public class Exam {
     private String username;
     private Integer score;
@@ -12,6 +14,34 @@ public class Exam {
         this.score = score;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.username);
+        hash = 97 * hash + Objects.hashCode(this.score);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Exam other = (Exam) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        return Objects.equals(this.score, other.score);
+    }
+    
+    
+    
     public String getUsername() {
         return username;
     }
--- src/main/java/com/day16/SetAndExam.java
+++ src/main/java/com/day16/SetAndExam.java
@@ -7,9 +7,14 @@ public class SetAndExam {
     public static void main(String[] args) {
         Exam exam1 = new Exam("John", 90);
         Exam exam2 = new Exam("Mary", 80);
+        Exam exam3 = new Exam("Mary", 80);
+        // 自建物件給Set集合時,記得要實作equals()與hashCode()方法
         Set<Exam> exams = new LinkedHashSet<>();
         exams.add(exam1);
         exams.add(exam2);
+        exams.add(exam3);
         System.out.println(exams);
+        System.out.println(exams.size());
+        
     }
 }
