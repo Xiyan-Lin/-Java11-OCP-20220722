3d1e3a63515a59c71e1f4dedd17ed0d847fa75b5
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-04 13:39:24 +0800

    java

---
+++ src/main/java/com/day15/ListDemo2.java
@@ -0,0 +1,24 @@
+package com.day15;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ListDemo2 {
+    public static void main(String[] args) {
+        // 佇列 Queue 先進先出
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+        System.out.println(queue);
+        System.out.println(queue.poll()); // 取得元素
+        System.out.println(queue);
+        System.out.println(queue.poll()); // 取得元素
+        System.out.println(queue);
+        System.out.println(queue.poll()); // 取得元素
+        System.out.println(queue);
+        System.out.println(queue.poll()); // 取得元素
+        System.out.println(queue);
+        
+    }
+}
