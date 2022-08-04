20b38b96c89375b1fc0ab384ec4620a111104db3
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-04 10:17:12 +0800

    java

--- src/main/java/com/day14/SetDemo1.java
+++ src/main/java/com/day14/SetDemo1.java
@@ -1,6 +1,7 @@
 package com.day14;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SetDemo1 {
@@ -15,10 +16,17 @@ public class SetDemo1 {
         set.add(70); // int 會自動轉 Integer (auto-boxing) -> Object
         set.add(null); // 放置 null
         System.out.println(set);
-        // 使用 for/each(in)
+        // 使用 for/each(in) (會自動往下尋訪)
         for(Object obj : set) {
             System.out.println(obj);
         }
-        
+        // 使用 Iterator 走訪器 (會自動往下尋訪)
+        Iterator iter = set.iterator();
+        while (iter.hasNext()) { // 判斷使否有元素 ?
+            Object obj = iter.next(); // 取得元素
+            System.out.println(obj);
+        }
+        // 使用 java 8 foreach
+        set.forEach(System.out::println);
     }
 }
