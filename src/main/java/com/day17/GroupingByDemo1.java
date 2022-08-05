c15e030059d67b2bb00a6995a9a8a0d6262208f5
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-05 14:06:12 +0800

    java

--- src/main/java/com/day17/GroupingByDemo1.java
+++ src/main/java/com/day17/GroupingByDemo1.java
@@ -3,6 +3,7 @@ package com.day17;
 import java.util.List;
 import java.util.Map;
 import static java.util.Arrays.asList;
+import java.util.Map.Entry;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.summingInt;
@@ -14,6 +15,15 @@ public class GroupingByDemo1 {
         List<String> items = asList("apple", "banana", "apple", "apple", "orange", "banana", "papaya");
         Map<String, Long> result = items.stream()
                                         .collect(groupingBy(identity(), counting()));
-        System.out.println(result);
+        System.out.println(result); // {papaya=1, orange=1, banana=2, apple=3}
+        // 加入排序
+        // 排序前: {papaya=1, orange=1, banana=2, apple=3}
+        // 排序後: {apple=3, banana=2, papaya=1, orange=1}
+        result.entrySet()
+              .stream()
+              .sorted(Entry.<String, Long>comparingByValue().reversed())
+              .forEachOrdered(e -> System.out.println(e));
+                
+                
     }
 }
