7db86de1db69be2ee1b7ca74500b38c310dec5d7
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-05 11:55:27 +0800

    java

--- src/main/java/com/day17/CollectDemo1.java
+++ src/main/java/com/day17/CollectDemo1.java
@@ -2,6 +2,7 @@ package com.day17;
 
 // collect() 收集器
 import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -12,8 +13,16 @@ public class CollectDemo1 {
         // J 開頭的統一集中到一個集合中
         Set<String> jnames = names.stream()
              .filter(name -> name.charAt(0) == 'J')
-             //.forEach(System.out::println);
+             .peek(System.out::println)
              .collect(toSet());
         System.out.println(jnames);
+        
+        List<Integer> scores = Arrays.asList(100, 40, 60, 80, 55, 100);
+        // 將及格的分數統一集中到一個集合中
+        List<Integer> passScores = scores.stream()
+              .filter(score -> score >= 60)
+              .peek(System.out::println)
+              .collect(toList());
+        System.out.println(passScores);
     }
 }
