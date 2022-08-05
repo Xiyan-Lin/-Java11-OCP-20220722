93f3030b3b926a65435cb0bd82c50c9019ba4bb4
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-05 11:38:40 +0800

    java

--- src/main/java/com/day17/MapDemo2.java
+++ src/main/java/com/day17/MapDemo2.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.day17;
 
-/**
- *
- * @author MB-teacher
- */
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Map;
+
 public class MapDemo2 {
-    
+    public static void main(String[] args) {
+        Map<String, Integer> map = new Hashtable<>();
+        map.put("A", 100);
+        map.put("B", 90);
+        map.put("C", 100);
+        System.out.println(map);
+        // 尋訪元素內容
+        Enumeration<Integer> e = ((Hashtable)map).elements();
+        while (e.hasMoreElements()) {
+            Integer value = e.nextElement();
+            System.out.println(value);
+        }
+        // 尋訪鍵值內容
+        Enumeration<String> e2 = ((Hashtable)map).keys();
+        while (e2.hasMoreElements()) {
+            String key = e2.nextElement();
+            System.out.println(key);
+        }
+        
+    }
 }
