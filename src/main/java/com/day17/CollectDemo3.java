fa5bf9dfadb6242a1929a23e978ada71feabc6ce
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-05 13:40:20 +0800

    java

--- src/main/java/com/day17/CollectDemo3.java
+++ src/main/java/com/day17/CollectDemo3.java
@@ -1,7 +1,11 @@
 package com.day17;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.summarizingInt;
+import static java.util.stream.Collectors.summingInt;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 public class CollectDemo3 {
@@ -17,5 +21,17 @@ public class CollectDemo3 {
         System.out.println(result3);
         String result4 = names.stream().collect(joining(", ", "begin-", "-end"));
         System.out.println(result4);
+        // counting() 對元素進行計數, 並將結果以 long 值回傳
+        long count = names.stream().collect(counting());
+        System.out.println(count);
+        // summarizeInt() 對所有元素進行加總
+        // 例如: 計算每一個名字的長度並加總
+        // summingInt 會回傳一個數值
+        // summarizingInt 會回傳一個統計物件
+        int summarize = names.stream().collect(summingInt(String::length));
+        System.out.println(summarize);
+        
+        IntSummaryStatistics stat = names.stream().collect(summarizingInt(String::length));
+        System.out.println(stat);
     }
 }
