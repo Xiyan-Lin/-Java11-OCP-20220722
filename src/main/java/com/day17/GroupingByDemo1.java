5a51221afacb884a48bc9f487949923c08917e34
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-05 14:07:47 +0800

    java

--- src/main/java/com/day17/GroupingByDemo1.java
+++ src/main/java/com/day17/GroupingByDemo1.java
@@ -2,11 +2,10 @@ package com.day17;
 
 import java.util.List;
 import java.util.Map;
-import static java.util.Arrays.asList;
 import java.util.Map.Entry;
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.summingInt;
 import static java.util.function.Function.identity;
 
 public class GroupingByDemo1 {
