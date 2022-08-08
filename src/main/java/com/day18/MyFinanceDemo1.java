9695e1f07fca6b93ce90844d32cf35ac966d8b92
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-08 11:33:20 +0800

    java

---
+++ src/main/java/com/day18/MyFinanceDemo1.java
@@ -0,0 +1,29 @@
+package com.day18;
+
+import java.io.IOException;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+public class MyFinanceDemo1 {
+    public static void main(String[] args) {
+        try {
+            Stock stock = YahooFinance.get("2330.TW");
+            String symbol = stock.getSymbol(); // 商品代號
+            String name = stock.getName(); // 商品名稱/公司名稱
+            double price = stock.getQuote().getPrice().doubleValue(); // 目前價格
+            double change = stock.getQuote().getChange().doubleValue(); // 目前漲跌
+            double changeInPercent = stock.getQuote().getChangeInPercent().doubleValue(); // 目前漲跌幅
+            String tradeTimeStr = stock.getQuote().getLastTradeTime().getTime().toString(); // 最後交易時間(字串格式呈現)
+            System.out.printf("商品代號: %s\n", symbol);
+            System.out.printf("商品名稱: %s\n", name);
+            System.out.printf("目前價格: %.2f\n", price);
+            System.out.printf("目前漲跌: %.2f\n", change);
+            System.out.printf("漲跌幅: %.2f%%\n", changeInPercent);
+            System.out.printf("交易時間: %s\n", tradeTimeStr);
+        } catch (IOException ex) {
+            System.out.println("錯誤訊息:" + ex);
+            System.out.println("連線到 Yahoo Finance 失敗!");
+        }
+        
+    }
+}
