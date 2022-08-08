69fdada3e7f7dd549ba681c8e37d5d54d786290c
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-08 14:58:14 +0800

    java

--- src/main/java/com/day18/MyFinanceDemo6.java
+++ src/main/java/com/day18/MyFinanceDemo6.java
@@ -5,7 +5,7 @@ import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 public class MyFinanceDemo6 {
-
+    
     public static void main(String[] args) {
         try {
             // 連續查多組商品
