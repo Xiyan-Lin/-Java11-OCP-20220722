e42153ec20f07d61eac2fa44c5e3c1aa74cde0f8
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-08 15:28:03 +0800

    java

--- src/test/java/test/day18/Area3ATesting.java
+++ src/test/java/test/day18/Area3ATesting.java
@@ -10,11 +10,11 @@ public class Area3ATesting {
     public void testing() {
         // 1. Arrange 安排測試物件與期望結果
         Area area = new Area();
-        int w = 10; // 底
-        int h = 5;  // 高
+        int w = 10; // 底 (測試參數)
+        int h = 5;  // 高 (測試參數)
         int excepted = 25; // 期望得到的值
         
-        // 2. Act 調用目標測試物件
+        // 2. Act 調用目標測試物件方法
         int actual = area.triangle(w, h); // 實際得到的值
         
         // 3. Assert 驗證測試結果
