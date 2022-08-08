fb996276197e36fb646453219a5549302788ddbd
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-08 15:26:35 +0800

    java

---
+++ src/test/java/test/day18/Area3ATesting.java
@@ -0,0 +1,24 @@
+package test.day18;
+
+// 進行標準 3A 測試
+import com.day18.Area;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Area3ATesting {
+    @Test
+    public void testing() {
+        // 1. Arrange 安排測試物件與期望結果
+        Area area = new Area();
+        int w = 10; // 底
+        int h = 5;  // 高
+        int excepted = 25; // 期望得到的值
+        
+        // 2. Act 調用目標測試物件
+        int actual = area.triangle(w, h); // 實際得到的值
+        
+        // 3. Assert 驗證測試結果
+        Assert.assertEquals(actual, excepted); // 實際得到的值 == 期望得到的值 ??
+        
+    }
+}
