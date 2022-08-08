2a6926f105050952bb916961cb11bab71c58fdaf
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-08 10:30:21 +0800

    java

--- src/main/java/com/day18/StringIntDemo.java
+++ src/main/java/com/day18/StringIntDemo.java
@@ -4,7 +4,9 @@ public class StringIntDemo {
     public static void main(String[] args) {
         // 如何檢查字串內的資料都是數字 ?
         // [0-9]+ 任意1~多個(+) 0~9 的資料
+        // \\d+ (效果同上) d 就是 decimal(十進制)
         String numStr = "40";
         System.out.println(numStr.matches("[0-9]+"));
+        System.out.println(numStr.matches("\\d+"));
     }
 }
