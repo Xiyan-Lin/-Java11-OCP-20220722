36a357884d414be29781ee19b709659bc09c22a3
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 11:11:06 +0800

    java

--- src/main/java/com/day19/Shower.java
+++ src/main/java/com/day19/Shower.java
@@ -4,9 +4,12 @@ class Father extends Thread {
     public void run() {
         System.out.println("爸爸準備洗澡發現沒有瓦斯了");
         System.out.println("爸爸打電話請瓦斯工人送瓦斯");
+        
         Worker worker = new Worker(); // 建立瓦斯工人執行緒
         worker.start(); // 瓦斯工人執行送瓦斯任務
         
+        System.out.println("爸爸開始洗澡");
+        System.out.println("爸爸洗完澡了");
     }
 }
 
