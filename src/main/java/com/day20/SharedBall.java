5009d5ae598c8bdc7894d39142398aac60f968d4
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 13:48:35 +0800

    java

--- src/main/java/com/day20/SharedBall.java
+++ src/main/java/com/day20/SharedBall.java
@@ -5,12 +5,8 @@ import java.util.Map;
 
 public class SharedBall implements Runnable {
     private int i = 1000;
-    private Map<String, Integer> map;
-    public SharedBall() {
-        map = new LinkedHashMap<>();
-        map.put("T1", 0);
-        map.put("T2", 0);
-    }        
+    private Map<String, Integer> map = new LinkedHashMap<>();
+    
     @Override
     public void run() {
         while(pickUp()) {
@@ -24,7 +20,7 @@ public class SharedBall implements Runnable {
         if(i <= 0) return false;
         String tName = Thread.currentThread().getName();
         System.out.printf("%s 拿了第 %d 號球\n", tName, i);
-        map.put(tName, map.get(tName) + 1);
+        map.put(tName, map.getOrDefault(tName, 0) + 1);
         i = i - 1;
         return i > 0;
     }
--- src/test/java/test/day20/SharedBallDemo.java
+++ src/test/java/test/day20/SharedBallDemo.java
@@ -5,9 +5,17 @@ import com.day20.SharedBall;
 public class SharedBallDemo {
     public static void main(String[] args) {
         SharedBall sharedBall = new SharedBall();
+        for(int i=65;i<=70;i++) {
+            Thread t = new Thread(sharedBall, (char)i + "");
+            t.start();
+        }
+        /*
         Thread t1 = new Thread(sharedBall, "T1");
         Thread t2 = new Thread(sharedBall, "T2");
+        Thread tt = new Thread(sharedBall, "TT");
         t1.start();
         t2.start();
+        tt.start();
+        */
     }
 }
