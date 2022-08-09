9207ea347d652fe40fc57f2e1c323d25eb6e505f
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 10:22:40 +0800

    java

--- src/test/java/test/day19/RaceDemo.java
+++ src/test/java/test/day19/RaceDemo.java
@@ -10,6 +10,9 @@ public class RaceDemo {
         // 將任務注入到執行緒中
         Thread t1 = new Thread(turtle, "烏龜");
         Thread t2 = new Thread(rabbit, "兔子");
+        // 調整權限 1~10 (數值越大越優先執行, 預設是5)
+        t1.setPriority(Thread.MAX_PRIORITY); // 10
+        t2.setPriority(Thread.MIN_PRIORITY); // 1
         // 執行任務
         t1.start();
         t2.start();
