ab4e73f62c553ad8f3a4322de992083f4ef439ec
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 16:03:49 +0800

    java

--- src/main/java/com/day20/Cookies.java
+++ src/main/java/com/day20/Cookies.java
@@ -5,7 +5,7 @@ public class Cookies {
     private boolean empty = true; // 預設盤子是空的
     
     public synchronized void eat(int n) {
-        if(empty) { // 若盤子為空
+        while(empty) { // 若盤子為空
             try {
                 wait(); // 進入等待
             } catch (InterruptedException e) {
@@ -13,11 +13,11 @@ public class Cookies {
         }
         System.out.printf("小狗吃了第 %d 片餅乾\n", n);
         empty = true; // 將盤子設定為空
-        notify(); // 喚醒正在等待的執行緒
+        notifyAll(); // 喚醒正在等待的執行緒
     }
     
     public synchronized void put(int n) {
-        if(!empty) { // 若盤子非空
+        while(!empty) { // 若盤子非空
             try {
                 wait(); // 進入等待
             } catch (InterruptedException e) {
@@ -25,6 +25,6 @@ public class Cookies {
         }
         System.out.printf("主人放了第 %d 片餅乾\n", n);
         empty = false; // 將盤子設定為非空
-        notify(); // 喚醒正在等待的執行緒
+        notifyAll(); // 喚醒正在等待的執行緒
     }
 }
