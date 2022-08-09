848d3d2978c5faf4a8bee8796db853020c20b013
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 09:59:39 +0800

    java

--- src/main/java/com/day19/Music.java
+++ src/main/java/com/day19/Music.java
@@ -9,7 +9,7 @@ public class Music extends Thread {
     
     public void job() {
         String tName = Thread.currentThread().getName(); // 取得執行緒名稱
-        for(int i=1;i<=1000_0000;i++) {
+        for(int i=1;;i++) { // 無窮 for 迴圈
             System.out.printf("%s 在 Music 播放第 %d 首歌\n", tName, i);
         }
     }
