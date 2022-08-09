f37b0cf74c7b1d27d3b5022416af1db8b53573ca
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 10:10:39 +0800

    java

---
+++ src/main/java/com/day19/Turtle.java
@@ -0,0 +1,13 @@
+package com.day19;
+
+public class Turtle implements Runnable {
+
+    @Override
+    public void run() {
+        String tName = Thread.currentThread().getName(); // 取得執行緒名稱
+        for(int i=1;i<=1000;i++) {
+            System.out.printf("%s 跑了 %d 步\n", tName, i);
+        }
+    }
+    
+}
