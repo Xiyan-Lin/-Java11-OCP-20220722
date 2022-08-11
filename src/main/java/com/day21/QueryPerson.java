e9647dcb030ce69a573d4454bd6e552d2e5d4fb5
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-11 15:31:34 +0800

    java

--- src/main/java/com/day21/QueryPerson.java
+++ src/main/java/com/day21/QueryPerson.java
@@ -18,12 +18,13 @@ public class QueryPerson {
         // 1. 載入驅動
         Class.forName("org.apache.derby.jdbc.ClientDriver");
         // 2. 建立 Connection, Statement, ResultSet
+        // 放入 try() 內的物件都會實作 AutoCloseable 介面
         try(Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);) {
             System.out.printf("%5s %5s %5s %5s %10s\n", "id", "name", "pwd", "age", "time");
             System.out.printf("-----------------------------------\n");
-            // 資料結果會被放入到 rs 物件內
+            // 3. 資料結果會被放入到 rs 物件內
             // 顯示 rs 內的資料
             while (rs.next()) {                
                 int id = rs.getInt("id");
@@ -33,8 +34,8 @@ public class QueryPerson {
                 Date createtime = rs.getDate("createtime");
                 System.out.printf("%5d %5s %5s %5d %10s\n", id, uname, pwd, age, createtime);
             }
-            
         } catch (Exception e) {
+            e.printStackTrace();
         }
         
     }
