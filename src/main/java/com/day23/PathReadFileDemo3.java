76ec2f53e4c0c5f751e44ce8d074a7f9659e0a85
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-12 13:52:10 +0800

    java

---
+++ src/main/java/com/day23/PathReadFileDemo3.java
@@ -0,0 +1,16 @@
+package com.day23;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PathReadFileDemo3 {
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get("src/main/java/com/day22/data/lotto.txt");
+        // 讀取資料: Files.readAllLines() 得到 List 集合
+        List<String> list = Files.readAllLines(path);
+        list.forEach(System.out::println);
+        
+    }
+}
