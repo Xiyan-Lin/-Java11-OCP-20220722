745790360268ce3b930d38b300c4abf441a240d8
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-12 11:53:34 +0800

    java

--- src/main/java/com/day22/BufferedReaderDemo.java
+++ src/main/java/com/day22/BufferedReaderDemo.java
@@ -2,10 +2,11 @@ package com.day22;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.charset.Charset;
 
 public class BufferedReaderDemo {
     public static void main(String[] args) {
-        try(FileReader fr = new FileReader("src/main/java/com/day22/data/lotto.txt");
+        try(FileReader fr = new FileReader("src/main/java/com/day22/data/lotto.txt", Charset.forName("utf-8"));
             BufferedReader br = new BufferedReader(fr);) {
             
             String data = null;
