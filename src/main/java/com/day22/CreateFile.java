9c6c232f2afc7bc8b137058206a40698dcc33784
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-12 10:04:03 +0800

    java

--- src/main/java/com/day22/CreateFile.java
+++ src/main/java/com/day22/CreateFile.java
@@ -7,10 +7,10 @@ public class CreateFile {
     // 希望在 src/main/java/com/day22/data 下建立一個 demo.txt 的檔案
     // 完整路徑: src/main/java/com/day22/data/demo.txt
     public static void main(String[] args) throws IOException {
-        File forderPath = new File("src/main/java/com/day22/data");
+        File folderPath = new File("src/main/java/com/day22/data");
         File filePath = new File("src/main/java/com/day22/data/demo.txt");
         // 檢查目錄
-        if(forderPath.exists()) {
+        if(folderPath.exists()) {
             // 檢查檔案
             if(filePath.exists()) {
                 System.out.println(filePath + " 檔案已經存在, 無須建立");
@@ -23,7 +23,7 @@ public class CreateFile {
                 }
             }
         } else {
-            System.out.println(forderPath + " 目錄不存在, 無法建立檔案");
+            System.out.println(folderPath + " 目錄不存在, 無法建立檔案");
         }
     }
 }
