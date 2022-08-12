3c943a59ca1265e35e526823852a0302dc488885
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-12 13:41:29 +0800

    java

---
+++ src/main/java/com/day23/PathReadFileDemo1.java
@@ -0,0 +1,17 @@
+package com.day23;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathReadFileDemo1 {
+    public static void main(String[] args) {
+        Path path = Paths.get("src/main/java/com/day22/data/lotto.txt");
+        // 讀取資料: FileReader -> BufferedReader
+        try(BufferedReader br = new BufferedReader(new FileReader(path.toString()))) {
+            br.lines().forEach(System.out::println);
+        } catch (Exception e) {
+        }
+    }
+}
